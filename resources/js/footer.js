7698fa4b4c2101bf10986364dc942d375013d4a7
sroya <shekeb.roya@hsr.ch>
2017-06-18 20:19:17 +0200

    store form value into localStorage

--- resources/js/footer.js
+++ resources/js/footer.js
@@ -12,7 +12,7 @@ window.onload = function() {
     if(listExist.length > 0) {
         getList();
     }
-}
+};
 
 /***************************************************************************
 *** FUNCTIONS ***/
@@ -21,36 +21,23 @@ window.onload = function() {
 function setData() {
     console.log("note.html");
 
-    var numberListItems = 0;
-    numberListItems += 1;
-    
     var titleValue = document.getElementById("title").value;
     var descriptionValue = document.getElementById("description").value;
     var importanceValue = document.querySelector('.rating:checked').value;
-    var finishValue = document.getElementById("date").value;
-    var createdValue = new Date();
+    var finishDateValue = document.getElementById("date").value;
 
-    console.log(createdValue);
+    var saveValuesInLocalStorage = new itemClass(titleValue, descriptionValue, importanceValue, finishDateValue);
 
-    //setList
-    var setList ={id: numberListItems, title: titleValue, description: descriptionValue, importance: importanceValue, finish: finishValue, created: createdValue};
-    localStorage.setItem('listItem', JSON.stringify(setList));
+    console.log(saveValuesInLocalStorage.setList);
 
-    //getList
-    var getList =JSON.parse(localStorage.getItem('listItem'));
-    console.log(getList.title);
 
-    // all local storage keys and values
-    for (var i = 0; i < localStorage.length; ++i) {
-        console.log(localStorage.getItem(localStorage.key(i)));
-    }
 
     // delete keys and values
     /*localStorage.clear();
     localStorage.removeItem('test');
     delete window.localStorage["test"];*/
 
-    alert(setList)
+
 
 };
 
@@ -58,8 +45,42 @@ function getList() {
     console.log("index.html");
 
 
+    //getList
+    for (var j = 0; j < localStorage.length; ++j) {
+        var getList = JSON.parse(localStorage.getItem('listItem-'+j));
+        console.log("getList: "+getList.title);
+    }
+
+    // all local storage keys and values
+    for (var i = 0; i < localStorage.length; ++i) {
+        console.log(localStorage.getItem(localStorage.key(i)));
+    }
+
+    for(var k=0; k < localStorage.length; k++) {
+        var key = localStorage.key(k);
+        var value = localStorage[key];
+        var tesct = 3;
+        console.log("key: "+key);
+        console.log("value: "+value);
+    }
+
 }
 
 /***************************************************************************
  *** CLASS ***/
 
+function itemClass(titleValue, descriptionValue, importanceValue, finishDateValue) {
+
+    this.titleValue = titleValue;
+    this.descriptionValue = descriptionValue;
+    this.importanceValue = importanceValue;
+    this.finishDateValue = finishDateValue;
+    this.numberListItems = localStorage.length;
+    this.listItem = 'listItem-' + this.numberListItems;
+    this.createdValue = new Date();
+
+    //setList
+    this.setList ={id: this.numberListItems, title: this.titleValue, description: this.descriptionValue, importance: this.importanceValue, finishDate: this.finishDateValue, created: this.createdValue, finished: false};
+    localStorage.setItem(this.listItem, JSON.stringify(this.setList));
+
+}
